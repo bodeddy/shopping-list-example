ae6958a7da789a4ead774745fc3c546b0b1994e9
bodeddy <107798427+bodeddy@users.noreply.github.com>
2026-01-15 20:10:55 +0000

    Get lists added to document with delete button
    Majority of this project is done: you can add items to the list, with
    each item accompanied by a delete button. Cursor also hops back into
    the input field. However, bullet point is not aligning with the text
    and button.

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("hello, world");
+const unorderedList = document.querySelector("ul");
+const input = document.querySelector("#item");
+const button = document.querySelector("button");
+
+function clickButton (event) {
+    // event.preventDefault();
+    button.addEventListener("click", () => {
+        const currentValue = input.value;
+        input.value = "";
+
+        const list = document.createElement("li");
+        const span = document.createElement("span");
+        const newButton = document.createElement("button");
+
+        unorderedList.appendChild(span);
+        unorderedList.appendChild(newButton)
+
+        span.textContent = currentValue;
+        newButton.textContent = "Delete";
+        unorderedList.appendChild(list);
+
+        newButton.addEventListener("click", () => {
+            list.remove();
+            span.remove();
+            newButton.remove();
+        })
+
+        input.focus();
+    });
+}
+
+clickButton();
+
+
